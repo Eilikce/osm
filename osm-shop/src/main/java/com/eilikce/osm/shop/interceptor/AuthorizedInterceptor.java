f567267cd2226e3b92196038ce2a289298d53f3f
Eilik <Eilik@Eilikce-Surface>
2018-03-21 23:47:59 +0800

    1.配置测试拦截器2.引入RedisTemplate操作模板

---
+++ osm-shop/src/main/java/com/eilikce/osm/shop/interceptor/AuthorizedInterceptor.java
@@ -0,0 +1,43 @@
+package com.eilikce.osm.shop.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 鉴权拦截器
+ * @author Eilik
+ *
+ */
+public class AuthorizedInterceptor implements HandlerInterceptor{
+
+	private static Logger logger = Logger.getLogger(AuthorizedInterceptor.class);
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+		logger.debug("拦截器预处理进入");
+		return false;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
