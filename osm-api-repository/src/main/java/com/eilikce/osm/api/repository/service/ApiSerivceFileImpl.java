0357b9e10441dcb187b6ac8386d229a90803e5d6
Eilik <Eilik@Eilikce-Surface>
2018-08-15 10:21:00 +0800

    修复spring boot打成jar包后加载包内资源文件失败的问题

--- osm-api-repository/src/main/java/com/eilikce/osm/api/repository/service/ApiSerivceFileImpl.java
+++ osm-api-repository/src/main/java/com/eilikce/osm/api/repository/service/ApiSerivceFileImpl.java
@@ -1,27 +1,29 @@
 package com.eilikce.osm.api.repository.service;
 
+import java.io.InputStream;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import com.eilikce.osm.api.repository.module.ApiView;
+import com.eilikce.osm.api.repository.util.ResourceLoader;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class ApiSerivceFileImpl implements ApiService {
 
-	@Value(value = "classpath:data/api-data.json")
-	private Resource data;
+	@Value("data/api-data.json")
+	private String apiDataPath;
 
 	@Override
 	public List<ApiView> apiData() {
 		
 		try {
 			ObjectMapper mapper = new ObjectMapper();
-			List<ApiView> list = mapper.readValue(data.getFile(), new TypeReference<List<ApiView>>() { });
+			InputStream apiStream = ResourceLoader.load(apiDataPath);
+			List<ApiView> list = mapper.readValue(apiStream, new TypeReference<List<ApiView>>() { });
 			return list;
 		} catch (Exception e) {
 			e.printStackTrace();
