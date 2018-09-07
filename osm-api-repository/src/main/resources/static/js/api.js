af70c655ac7c5ed598a81e7dc78ab02006706a34
Eilikce <wanghw_eilikce@163.com>
2018-09-07 23:24:21 +0800

    取消文件读取功能，以页面直接存储

--- osm-api-repository/src/main/resources/static/js/api.js
+++ osm-api-repository/src/main/resources/static/js/api.js
@@ -10,64 +10,13 @@ $(function() {// 初始化内容
  * api数据初始化
  */
 function apiDataInit(){
-	//ajax获取数据
-	var apiJsonArr = apiDataAjax();
-	//dom拼接
-	var dom = apiDomCreate(apiJsonArr);
-	
-	//填充数据
-	$("#accordion").append(dom);
+	//格式化所有json数据
+	$(".json-data").each(function (e) {
+		var jsonData = $(this).text();
+        $(this).text(formatJson(jsonData));
+    })
+
 }
-
-/**
- * 根据数据拼接dom
- * @param apiJsonArr
- */
-function apiDomCreate(apiJsonArr){
-	
-	var dom = "";
-	
-	for(i in apiJsonArr){
-		var apiPath = apiJsonArr[i].api;
-		var apiMethodType = apiJsonArr[i].methodType;
-		var apiDesc = apiJsonArr[i].desc;
-		var apiParam = apiJsonArr[i].param;
-		var apiExample = formatJson(apiJsonArr[i].returnValue);
-		
-		dom += 
-		'<h3><span>'+apiMethodType+'</span><span class="api-name" >'+apiPath+'</span></h3>'+
-		'<div>'+
-			'<span class="api-desc-title">Description:</span>'+
-			'<pre class="api-desc-content">'+apiDesc+'</pre>'+
-			'<span class="api-desc-title">Param:</span>'+
-			'<pre class="api-desc-content">'+apiParam+'</pre>'+
-			'<span class="api-desc-title">ReturnValue Example:</span>'+
-			'<pre class="api-desc-content">'+apiExample+'</pre>'+
-		'</div>';
-		
-	}
-	
-	return dom;
-}
-
-/**
- * 请求后台数据，返回数据数组
- */
-function apiDataAjax(){
-	var apiJsonArr;
-	$.ajax({
-		type: "GET",
-		url:"api/apiData",
-		dataType:"json",
-		async:false,
-		success: function(data){
-			apiJsonArr = data;
-		}
-	});
-	return apiJsonArr; 
-}
-
-
 
 /**
  * jquery ui 初始化
